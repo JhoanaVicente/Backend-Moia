d79831accff938f0f5aa72f9407149d144411773
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-07-25 12:33:37 +0200

    create InscripcioController

--- src/main/kotlin/controllers/InscripcioController.kt
+++ src/main/kotlin/controllers/InscripcioController.kt
@@ -1,2 +1,16 @@
 package controllers
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import repositories.Inscripcio
+import repositories.InscripcioRepository
+
+@RestController
+class InscripcioController (@Autowired private val inscripcioRepository: InscripcioRepository){
+    @PostMapping("/inscripcions")
+    fun addInscripcio(@RequestBody inscripcio: Inscripcio): Inscripcio? {
+        return inscripcioRepository.save(inscripcio)
+    }
+}
